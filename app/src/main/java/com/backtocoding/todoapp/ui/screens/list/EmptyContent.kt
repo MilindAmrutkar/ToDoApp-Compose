63496d6caef2af6ff67f40704643f8e23dadb12d
MilindAmrutkar <milind27.amrutkar@gmail.com>
2023-09-12 21:59:01 +0530

    Added: EmptyContent composable

---
+++ app/src/main/java/com/backtocoding/todoapp/ui/screens/list/EmptyContent.kt
@@ -0,0 +1,43 @@
+package com.backtocoding.todoapp.ui.screens.list
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.backtocoding.todoapp.R
+import com.backtocoding.todoapp.ui.theme.MediumGray
+
+@Composable
+fun EmptyContent() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Icon(
+            modifier = Modifier.size(120.dp),
+            painter = painterResource(id = R.drawable.ic_sad_face),
+            contentDescription = stringResource(R.string.sad_face_icon),
+            tint = MediumGray
+        )
+        Text(
+            text = stringResource(R.string.empty_content),
+            color = MediumGray,
+            fontWeight = FontWeight.Bold,
+            fontSize = MaterialTheme.typography.headlineLarge.fontSize
+        )
+    }
+}
--- app/src/main/java/com/backtocoding/todoapp/ui/screens/list/ListContent.kt
+++ app/src/main/java/com/backtocoding/todoapp/ui/screens/list/ListContent.kt
@@ -5,9 +5,8 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -31,6 +30,18 @@ import com.backtocoding.todoapp.ui.theme.taskItemTextColor
 
 @Composable
 fun ListContent(
+    tasks: List<ToDoTask>,
+    navigateToTaskScreen: (taskId: Int) -> Unit
+) {
+    if (tasks.isEmpty()) {
+        EmptyContent()
+    } else {
+        DisplayTasks(tasks = tasks, navigateToTaskScreen = navigateToTaskScreen)
+    }
+}
+
+@Composable
+fun DisplayTasks(
     tasks: List<ToDoTask>,
     navigateToTaskScreen: (taskId: Int) -> Unit
 ) {
@@ -86,8 +97,7 @@ fun TaskItem(
                 ) {
                     Canvas(
                         modifier = Modifier
-                            .width(PRIORITY_INDICATOR_SIZE)
-                            .height(PRIORITY_INDICATOR_SIZE)
+                            .size(PRIORITY_INDICATOR_SIZE)
                     ) {
                         drawCircle(color = toDoTask.priority.color)
                     }
